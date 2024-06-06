7b47d885bc52bd718fcd773c8d53480eda4560f8
mal-nushi-dev <malnushi1999+dev@gmail.com>
2024-06-06 00:00:46 -0400

    added a dataframe manager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scripts.logging_config import get_logger
 from scripts.get_config import get_config
+from scripts.dataframe_manager import row_count_validation, column_validation, column_level_validation
 
 logger = get_logger()
 
@@ -11,40 +12,8 @@ source_df = pd.read_csv(source_file)
 target_df = pd.read_csv(target_file)
 
 # Check counts
-source_record_count = source_df.shape[0]
-target_record_count = target_df.shape[0]
-logger.info(f'Source record count: {source_record_count}')
-logger.info(f'Target record count: {target_record_count}')
-if source_record_count == target_record_count:
-    logger.info('Counts are the same')
-else:
-    logger.warning('Counts are different')
+row_count_validation(source_df, target_df)
 
 # Check columns
-source_col_count = source_df.columns
-target_col_count = target_df.columns
-logger.info(f'Source columns: {len(source_col_count)}')
-logger.info(f'Target columns: {len(target_col_count)}')
-if len(source_col_count) == len(target_col_count):
-    logger.info('Column counts are the same.')
-else:
-    logger.warning('Column counts are different.')
-
-missing_source_cols = set(source_col_count) - set(target_col_count)
-missing_target_cols = set(target_col_count) - set(source_col_count)
-
-if missing_source_cols:
-    logger.warning(f"Columns missing in DataFrame 1: {missing_source_cols}")
-if missing_target_cols:
-    logger.warning(f"Columns missing in DataFrame 2: {missing_target_cols}")
-
-if not missing_source_cols and not missing_target_cols:
-    logger.info("All columns match")
-
-# Merge the two dataframes, indicator=True adds a column '_merge' that indicates where the merge matched
-merged_df = pd.merge(source_df, target_df, how='outer', indicator=True)
-
-# Rows that are only in source_df or only in target_df
-diff_df = merged_df[merged_df['_merge'] != 'both']
-
-pd.DataFrame.to_html(diff_df, 'assets/outputs/diff.html')
+column_validation(source_df, target_df)
+column_level_validation(source_df, target_df)
---
+++ scripts/dataframe_manager.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from scripts.logging_config import get_logger
+
+
+logger = get_logger()
+
+
+def row_count_validation(df1, df2):
+    # Check counts
+    df1_record_count = df1.shape[0]
+    df2_record_count = df2.shape[0]
+
+    logger.info(f"Source record count: {df1_record_count}")
+    logger.info(f"Target record count: {df2_record_count}")
+
+    if df1_record_count == df2_record_count:
+        logger.info("Counts are the same")
+    else:
+        logger.warning("Counts are different")
+
+
+def column_validation(df1, df2):
+    df1_record_count = df1.columns
+    df2_record_count = df2.columns
+
+    # Check column counts
+    logger.info(f"Source columns: {len(df1_record_count)}.")
+    logger.info(f"Target columns: {len(df2_record_count)}.")
+    if len(df1_record_count) == len(df2_record_count):
+        logger.info("Column counts are the same.")
+    else:
+        logger.warning("Column counts are different.")
+
+    # Check for missing columns in each DataFrame
+    missing_df1_columns = set(df1_record_count) - set(df2_record_count)
+    missing_df2_columns = set(df2_record_count) - set(df1_record_count)
+    if missing_df1_columns:
+        logger.warning(f"Columns missing in DataFrame 1: {missing_df1_columns}.")
+    if missing_df2_columns:
+        logger.warning(f"Columns missing in DataFrame 2: {missing_df2_columns}.")
+
+    if not missing_df1_columns and not missing_df2_columns:
+        logger.info("All columns match.")
+
+
+def column_level_validation(df1, df2):
+    # Merge the two dataframes, indicator=True adds a column '_merge' that indicates where the merge matched
+    merged_df = pd.merge(df1, df2, how='outer', indicator=True)
+
+    # Rows that are only in source_df or only in target_df
+    diff_df = merged_df[merged_df['_merge'] != 'both']
+    if diff_df.empty:
+        logger.info("There are no differences in the data.")
+    else:
+        pd.DataFrame.to_html(diff_df, 'assets/outputs/diff.html')
